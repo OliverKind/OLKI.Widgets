d259b8da936475f613c643f3dc8bdcd26c773fc5
Oliver Kind <github@oliver-kind.de>
2020-08-02 14:28:34 +0200

    Updated to version 5.0.0.7

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("4.0.0.6")]
-[assembly: AssemblyFileVersion("4.0.0.6")]
+[assembly: AssemblyVersion("5.0.0.7")]
+[assembly: AssemblyFileVersion("5.0.0.7")]
 [assembly: Guid ("7dd1e2ef-d1ee-4b33-8c2a-41770b55e9a4")]
--- README.md
+++ README.md
@@ -9,6 +9,7 @@ These instructions will get you a copy of the project up and running on your loc
 ### Prerequisites
 
 * .NET Framework 4.7.2
+* OLKI.Tools.ColorAndPicture 1.0.0.1 or higher
 
 ### Installing
 
